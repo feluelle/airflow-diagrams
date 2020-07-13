b34dbd5b8a0464247b37125d6075cad631f73b88
feluelle <felix.uellendall@publicispixelpark.de>
2020-07-13 12:40:31 +0200

    Add mapping for all available 1.10.x operators

--- dev/scripts/init_mapping.py
+++ dev/scripts/init_mapping.py
@@ -10,7 +10,7 @@ with open('../../airflow_diagrams/mapping.json', 'r') as json_file:
     existing_operators = json.load(json_file)
 
 for operator in all_operators:
-    existing_operators.setdefault(operator, dict(provider='', resource_type='', name=''))
+    existing_operators.setdefault(operator, dict(provider='generic', resource_type='blank', name='Blank'))
 existing_operators = dict(sorted(existing_operators.items()))
 
 with open('../../airflow_diagrams/mapping.json', 'w') as json_file:
