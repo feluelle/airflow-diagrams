3964e8d68e5b584326b502e72bc474c2fbad0739
Felix Uellendall <feluelle@pm.me>
2022-01-24 21:36:08 +0100

    Add s3 abbreviation

--- examples/mails_to_postgres_diagrams.py
+++ examples/mails_to_postgres_diagrams.py
@@ -1,7 +1,7 @@
 from diagrams import Diagram
-from diagrams.generic.blank import Blank
+from diagrams.aws.storage import SimpleStorageServiceS3
 
 with Diagram("mails_to_postgres", show=False):
-    _5b3a2bc96456974068c7fdf8560d2e4e = Blank("transfer_imap_mails_to_s3")
+    _5b3a2bc96456974068c7fdf8560d2e4e = SimpleStorageServiceS3("transfer_imap_mails_to_s3")
     
     
