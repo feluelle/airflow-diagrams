b9ef111a42ead0f2b42fc8fcacbc2ce29a55b4a0
Felix Uellendall <feluelle@pm.me>
2022-01-24 21:34:32 +0100

    Set fallback node to programming.flowchart.Action

--- examples/airbyte_diagrams.py
+++ examples/airbyte_diagrams.py
@@ -1,9 +1,9 @@
 from diagrams import Diagram
-from diagrams.generic.blank import Blank
+from diagrams.programming.flowchart import Action
 
 with Diagram("airbyte", show=False):
-    _6779f45be4c8a58feed5ddfda70e2382 = Blank("airbyte_job_sensor")
-    _30e82bea0c5181aad98d772e9133b66b = Blank("airbyte_trigger_async")
+    _6779f45be4c8a58feed5ddfda70e2382 = Action("airbyte_job_sensor")
+    _30e82bea0c5181aad98d772e9133b66b = Action("airbyte_trigger_async")
     
     _30e82bea0c5181aad98d772e9133b66b >> [_6779f45be4c8a58feed5ddfda70e2382]
     
