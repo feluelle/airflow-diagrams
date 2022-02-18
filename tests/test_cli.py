c05f07d6ebbbf0c4078c387e2e44057faaec3211
Felix Uellendall <feluelle@pm.me>
2022-02-18 08:35:06 +0100

    Fix tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,33 @@
+from unittest.mock import PropertyMock
+
 import pytest
+from blessed import Terminal
 from typer.testing import CliRunner
 
 from airflow_diagrams import __app_name__, __version__, cli
 
 runner = CliRunner()
+
+
+@pytest.fixture
+def mock_term(mocker):
+    mock_get_location = mocker.patch.object(
+        Terminal,
+        "get_location",
+        side_effect=[(0, 0), (3, 0)],
+    )
+    mock_move_up = mocker.patch.object(Terminal, "move_up", return_value="\033[A")
+    mock_clear_eol = mocker.patch.object(
+        Terminal,
+        "clear_eol",
+        new_callable=PropertyMock,
+        create=True,
+        return_value="\033[K",
+    )
+    yield
+    assert mock_get_location.call_count == 2
+    assert mock_move_up.call_count == 1
+    assert mock_clear_eol.call_count == 1
 
 
 @pytest.fixture
@@ -36,14 +60,16 @@ def test_version():
     assert f"{__app_name__} v{__version__}\n" == result.stdout
 
 
-def test_generate(mock_dag):
+def test_generate(mock_dag, mock_term, mocker):
     """Test end-to-end"""
+    mocker.patch("time.sleep", return_value=None)
     result = runner.invoke(cli.app, ["generate", "--output-path", "generated/"])
     assert result.exit_code == 0
     assert (
         "ℹ️ Retrieved Airflow DAG test_dag.\n"
         "  ℹ️ Retrieved Airflow Task test_task (module.path.ClassName) with downstream tasks [].\n"
         "  🔮Found match programming.flowchart.Action.\n"
+        "\x1b[A\x1b[K\x1b[A\x1b[K\x1b[A\x1b[K\n"
         "🪄 Generated diagrams file generated/test_dag_diagrams.py.\n"
         "Done. 🎉\n"
     ) == result.stdout
@@ -57,3 +83,10 @@ def test_generate_with_verbose(mock_dag):
     )
     assert result.exit_code == 0
     assert result.stdout.startswith("💬 Running with verbose output..")
+    assert (
+        "ℹ️ Retrieved Airflow DAG test_dag.\n"
+        "  ℹ️ Retrieved Airflow Task test_task (module.path.ClassName) with downstream tasks [].\n"
+        "  🔮Found match programming.flowchart.Action.\n"
+        "🪄 Generated diagrams file generated/test_dag_diagrams.py.\n"
+        "Done. 🎉\n"
+    ) in result.stdout
