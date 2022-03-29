b57335f7854bd688f5b404090eeb4d3cc1e4cb4e
Felix Uellendall <feluelle@users.noreply.github.com>
2022-03-29 08:41:55 +0200

    Auto-update usage (#51)

---
+++ dev/usage/usage_generator.py
@@ -0,0 +1,16 @@
+import sys
+
+import requests
+
+if __name__ == "__main__":
+    response = requests.post(
+        url="https://carbonara-42.herokuapp.com/api/cook",
+        json={
+            "code": sys.stdin.read(),
+            "language": "plaintext",
+            "backgroundColor": "transparent",
+        },
+    )
+
+    with open("assets/images/usage.png", "wb") as file:
+        file.write(response.content)
