c402a4775ad859511b2471f1d76900d1a63a1fb8
Felix Uellendall <feluelle@pm.me>
2022-06-14 09:11:40 +0200

    Refactor code
    - use pytest mark parametrize for testing cli
    - add mapper command to dev scripts

--- dev/airflow/airflow_dags_creator.py
+++ dev/airflow/airflow_dags_creator.py
@@ -1,4 +1,5 @@
 """Creates random fake Airflow DAGs as yaml file."""
+import argparse
 import random
 
 import yaml
@@ -9,15 +10,12 @@ from airflow_diagrams.class_ref import ClassRef, retrieve_class_refs
 
 
 def _retrieve_airflow_class_refs():
-    class_refs = (
-        retrieve_class_refs(directory="generated/airflow/airflow/operators")
-        + retrieve_class_refs(directory="generated/airflow/airflow/sensors")
-        + retrieve_class_refs(directory="generated/airflow/airflow/providers")
-    )
+    class_refs = retrieve_class_refs(directory="generated/airflow")
     return list(
         filter(
             lambda class_ref: (
-                (
+                class_ref.module_path.startswith("airflow.")
+                and (
                     ".operators." in class_ref.module_path
                     or ".sensors." in class_ref.module_path
                     or ".transfers." in class_ref.module_path
@@ -29,9 +27,8 @@ def _retrieve_airflow_class_refs():
     )
 
 
-def _generate_airflow_tasks(n: int = 20):
-    class_refs = random.choices(_retrieve_airflow_class_refs(), k=n)  # nosec
-    graph = gnp_random_graph(n, 0.05, directed=True)
+def _generate_airflow_tasks(class_refs: list[ClassRef]):
+    graph = gnp_random_graph(len(class_refs), 0.05, directed=True)
     dag = DiGraph(
         [
             (
@@ -45,21 +42,43 @@ def _generate_airflow_tasks(n: int = 20):
     )
     tasks_with_downstream_tasks = to_dict_of_lists(dag)
 
-    return [
-        AirflowTask(
-            class_ref=ClassRef.from_string(key),
-            task_id=key,
-            downstream_task_ids=value,
-            group_name=None,
-        )
-        for key, value in tasks_with_downstream_tasks.items()
-    ]
+    return tasks_with_downstream_tasks
 
 
 if __name__ == "__main__":
-    airflow_dags = dict(
-        random_dag=_generate_airflow_tasks(),
-    )
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o")
+    parser.add_argument("-a", action="store_true")
+    args = parser.parse_args()
 
-    with open("generated/airflow_dags_random.yml", "w") as file:
+    class_refs = _retrieve_airflow_class_refs()
+
+    if args.a:
+        airflow_dags = dict(
+            dag=[
+                AirflowTask(
+                    class_ref=ClassRef.from_string(key),
+                    task_id=key,
+                    downstream_task_ids=[],
+                    group_name=None,
+                )
+                for key, value in _generate_airflow_tasks(class_refs).items()
+            ],
+        )
+    else:
+        airflow_dags = dict(
+            random_dag=[
+                AirflowTask(
+                    class_ref=ClassRef.from_string(key),
+                    task_id=key,
+                    downstream_task_ids=value,
+                    group_name=None,
+                )
+                for key, value in _generate_airflow_tasks(
+                    random.choices(class_refs, k=20),  # nosec
+                ).items()
+            ],
+        )
+
+    with open(args.o, "w") as file:
         yaml.dump(airflow_dags, file)
